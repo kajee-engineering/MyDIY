0f00c5a0d3885f75fd13f9266388526fb3152e41
Tomonari Kaji <kaji.tomonari@gmail.com>
2025-06-28 10:46:56 +0900

    リファクタ

--- MyDIY/src/dev/lpa/FindPermutation.java
+++ MyDIY/src/dev/lpa/FindPermutation.java
@@ -13,7 +13,6 @@ public class FindPermutation {
 
         List<Integer> result = new ArrayList<>();
 
-        // bのスタートを先頭から1ずつ右へスライドする
         // sの文字数未満はfalseのため処理しない
         for (int i = 0; i <= b.length() - s.length(); i++) {
 
@@ -21,27 +20,25 @@ public class FindPermutation {
             if (sub.contains("d")) {
                 continue;
             }
-
             if (isPermutation(s, sub)) {
                 result.add(i);
             }
-            ;
         }
 
         System.out.println("各順列の場所は" + result + "です");
     }
 
-    private static int charToIndex(char c) {
-        return c - 'a'; // aは97である。したがってa - a = 0 になる。
-    }
-
     private static boolean isPermutation(String s, String sub) {
+
+        // 固定長配列の配列の長さを短くするための無名関数
+        java.util.function.IntUnaryOperator charToIndex = c -> c - 'a'; // aは97である。したがってa - a = 0 になる。
+
         int[] sCounts = new int[4];
         int[] subCounts = new int[4];
 
         for (int i = 0; i < s.length(); i++) {
-            sCounts[charToIndex(s.charAt(i))]++;
-            subCounts[charToIndex(sub.charAt(i))]++;
+            sCounts[charToIndex.applyAsInt(s.charAt(i))]++;
+            subCounts[charToIndex.applyAsInt(sub.charAt(i))]++;
         }
 
         for (int i = 0; i < 4; i++) {
