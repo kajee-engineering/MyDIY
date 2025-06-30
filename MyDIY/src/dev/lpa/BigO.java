9fc9342e7dcac3d9c650339f69faa04c543678d4
Tomonari Kaji <kaji.tomonari@gmail.com>
2025-07-01 06:51:58 +0900

    BigO

--- MyDIY/src/dev/lpa/BigO.java
+++ MyDIY/src/dev/lpa/BigO.java
@@ -21,6 +21,10 @@ public class BigO {
 
     public static void main(String[] args) {
         System.out.println("Total : " + sum(5));
+        System.out.println(twoSum());
+        System.out.println(twoTimes());
+        System.out.println(f(3));
+        System.out.println(f(4));
     }
 
     static int sum(int n) {
@@ -29,7 +33,61 @@ public class BigO {
             return 0;
         }
         callStack++;
-        return n + sum(n-1); // 呼び出しごとにコールスタックに積まれる。つまり実際のメモリを消費します。
+        return n + sum(n - 1); // 呼び出しごとにコールスタックに積まれる。つまり実際のメモリを消費します。
     }
+
+    // 実行時間を足す場合
+    static boolean twoSum() {
+        int[] arrA = {1, 5, 3, 7};
+        int[] arrB = {2, 8, 4, 6};
+        // 処理Aを行い、
+        for (int a : arrA) {
+            System.out.print(a + " ");
+        }
+        // 処理Aが終わってから
+        System.out.println("");
+        // 処理Bを行う
+        for (int b : arrB) {
+            System.out.print(b + " ");
+        }
+        System.out.println("");
+        return false;
+        // この場合は実行時間の和
+    }
+
+    // 実行時間をかける場合
+    static boolean twoTimes() {
+        int[] arrA = {1, 5, 3, 7};
+        int[] arrB = {2, 8, 4, 6};
+        // 処理Aを行う度に
+        for (int a : arrA) {
+            System.out.print(a + " ");
+            // 処理Bを行う場合は
+            for (int b : arrB) {
+                System.out.print(b + " ");
+            }
+        }
+        return false;
+        // この実行時間は積
+    }
+
+    // 再帰の実行時間
+    // n(4) = n(3) + n(3) -> 8
+    // n(3) = n(2) + n(2) -> 4
+    // n(2) = n(1) + n(1) -> 2
+    // n(1) -> 1
+    static int f(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+        return f(n - 1) + f(n - 1);
+    }
+    // 複数の呼び出しを行う再帰関数では呼び出し回数の数になることが多い
+    // n(1) = 1 = 2^0
+    // n(2) = 2 = 2^1
+    // n(4) = 3 = 2^2
+    // n(8) = 4 = 2^3
+    // つまりO(2^n)
+    // しかし同時に全ての呼び出しが発生するわけではないので空間計算量はO(n)です。
 }
 
